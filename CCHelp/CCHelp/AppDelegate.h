b050c616b6b4225781727eafd20cdaa399747a2a
chenchao <chenchao@tataufo.com>
2016-07-11 20:11:54 +0800

    create new project

---
+++ CCHelp/CCHelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCHelp
+//
+//  Created by chenchao on 16/7/11.
+//  Copyright © 2016年 chenchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCHelp/CCHelp/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CCHelp
+//
+//  Created by chenchao on 16/7/11.
+//  Copyright © 2016年 chenchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
